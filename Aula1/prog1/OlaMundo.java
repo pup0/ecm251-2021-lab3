ef9c21ba4949bffee25e0db8a8c5055a1e89ddfe
pup0 <conrado.azzalin@uol.com.br>
2021-02-26 13:39:41 -0300

    aula 2 teoria revisada

--- Aula1/prog1/OlaMundo.java
+++ Aula1/prog1/OlaMundo.java
@@ -2,7 +2,14 @@ package Aula1.prog1;
 
 class OlaMundo{
     public static void main(String[] args) {
+        //comentário de uma linha
         System.out.println("Ola Mundo!");
+        /*
+            comentários de vaaaaaaaaaaarias linhas
+
+
+        */
         System.out.println("Kim Jon Un é mau?");
+        System.out.println("Mas o que é isso?!?!");
     }
 }
